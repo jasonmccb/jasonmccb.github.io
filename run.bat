8e0ad090982620f5f32c3120c3b6b251045afec5
jasonmccb <jasonmccb@163.com>
2024-10-31 21:34:31 +1300

    Add a new post

--- run.bat
+++ run.bat
@@ -2,6 +2,6 @@ D:
 cd D:\JekyllBlog\myblog
 call push_to_Algolia.bat	REM Must use "call" otherwise sub bat won't come back
 ::TCP   127.0.0.1:4000    0.0.0.0:0    LISTENING   5740  [FoxitProtect.exe]
-sc stop FxService
+::sc stop FxService
 jekyll server
 ::bundle exec jekyll serve --watch
